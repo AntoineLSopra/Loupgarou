477826ab9c323c2a22c318d1d7bc10b4df83396a
AntoineL <antoine.lavie@soprasteria.com>
2019-01-10 11:17:38 +0100

    Test

--- loupgarou-data-jpa/src/main/java/fr/loupgarou/datajpa/IDAOUtilisateur.java
+++ loupgarou-data-jpa/src/main/java/fr/loupgarou/datajpa/IDAOUtilisateur.java
@@ -3,11 +3,12 @@ package fr.loupgarou.datajpa;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import fr.loupgarou.model.*;
 
 public interface IDAOUtilisateur extends JpaRepository<Utilisateur, Integer>{
-
+	@Query("")
 	public int connexion(String use, String pass);
 
 }
