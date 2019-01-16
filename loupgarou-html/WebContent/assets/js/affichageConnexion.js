9d841f9087b4d9d96f40e052738a37f50ebf3f2f
AntoineL <antoine.lavie@soprasteria.com>
2019-01-16 09:43:58 +0100

    Ajout de l'affichage en JS
    En cliquant sur les boutons, on affiche le contenu désigné.

---
+++ loupgarou-html/WebContent/assets/js/affichageConnexion.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+$('div a').bind('click', function() {
+//	alert("A MALIBU");
+//	this -> lien cliqué
+//	alert($(this).attr('href'));
+	
+	let mySelecteurAAfficher = $(this).attr('href');
+	$('section').hide();
+	$(mySelecteurAAfficher).show(); // -> Pareil que $($(this).attr('href')).show();
+	return false;
+})
