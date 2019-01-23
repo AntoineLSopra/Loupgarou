80b87d5d596e321d8e9ea66345fb91ee1a2e1458
Gibert <maxime.gibert@sfr.fr>
2019-01-23 11:54:29 +0100

    Création Loupgarou-web

---
+++ loupgarou-web/src/main/java/fr/loupgarou/config/WebConfig.java
@@ -0,0 +1,67 @@
+package fr.loupgarou.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+@Configuration
+@ComponentScan("fr.formation")
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+	
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		
+	registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+	registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+	
+	}
+	
+	
+//	@Bean
+//	public UrlBasedViewResolver viewResolver() {
+//	UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+//	viewResolver.setViewClass(JstlView.class);
+//	viewResolver.setPrefix("/WEB-INF/views/");
+//	viewResolver.setSuffix(".jsp");
+//	return viewResolver;
+//	}
+//	
+	
+	
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+	SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+	templateResolver.setPrefix("/WEB-INF/templates/");
+	templateResolver.setSuffix(".html");
+	return templateResolver;
+	}
+	
+	@Bean
+	public SpringTemplateEngine templateEngine(SpringResourceTemplateResolver templateResolver) {
+	SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+	templateEngine.setTemplateResolver(templateResolver);
+	templateEngine.setEnableSpringELCompiler(true);
+	templateEngine.addDialect(new LayoutDialect());
+	return templateEngine;
+	}
+	
+
+	@Bean
+	public ThymeleafViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine);
+		return viewResolver;
+	}
+	
+}
