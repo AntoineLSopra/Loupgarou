4d1e22d433100a6beff627705866e5a8be158d0a
Gibert <maxime.gibert@sfr.fr>
2019-01-24 12:36:33 +0100

    Demarrage partie + affichage participations

--- loupgarou-data-jpa/src/main/java/fr/loupgarou/datajpa/IDAOParticipation.java
+++ loupgarou-data-jpa/src/main/java/fr/loupgarou/datajpa/IDAOParticipation.java
@@ -1,10 +1,18 @@
 package fr.loupgarou.datajpa;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import fr.loupgarou.model.Participation;
+import fr.loupgarou.model.Partie;
+import fr.loupgarou.model.Personnage;
 
 public interface IDAOParticipation extends JpaRepository<Participation, Integer>{
 
-	
+
+	public List<Participation> findByPartieId(int id);
+
 }
