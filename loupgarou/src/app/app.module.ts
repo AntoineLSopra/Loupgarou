8038a46f655016ac01b6057b49b66e8bcee72fbc
AntoineL <antoine.lavie@soprasteria.com>
2019-02-04 15:21:42 +0100

    Rédaction app.module.ts + ajout plateau-component
    On est content

--- loupgarou/src/app/app.module.ts
+++ loupgarou/src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule , Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { SprBoldComponent } from './spr-bold/spr-bold.component';
+import { SprBoldProduitComponent } from './spr-bold-produit/spr-bold-produit.component';
+import { SprTooltipComponent } from './spr-tooltip/spr-tooltip.component';
+import { PlateauComponent } from './plateau/plateau.component';
+
+const routes: Routes = [
+  { path: 'plateau', component: PlateauComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SprBoldComponent,
+    SprBoldProduitComponent,
+    SprTooltipComponent,
+    PlateauComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
