cbbfbfcf6d041e69877f299fea9dd119468458f1
Gibert <maxime.gibert@sfr.fr>
2019-02-04 15:22:09 +0100

    Rest Controller

--- loupgarou-boot/src/main/java/fr/loupgarou/restcontroller/PlateauRestController.java
+++ loupgarou-boot/src/main/java/fr/loupgarou/restcontroller/PlateauRestController.java
@@ -1,15 +1,20 @@
 package fr.loupgarou.restcontroller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.annotation.JsonView;
 import fr.loupgarou.dao.IDAOParticipation;
+import fr.loupgarou.dao.IDAOPersonnage;
 import fr.loupgarou.model.Participation;
+import fr.loupgarou.model.Personnage;
 import fr.loupgarou.projection.Views;
 
 
@@ -20,6 +25,16 @@ public class PlateauRestController {
 	@Autowired
 	private IDAOParticipation daoParticipation;
 	
+	@Autowired
+	private IDAOPersonnage daoPersonnage;
+	
+
+	@GetMapping("/api/personnages")
+	@JsonView(Views.Personnage.class)
+	public List<Personnage> listeRest() {
+
+		return this.daoPersonnage.findAll();
+	}
 
 	@DeleteMapping("/api/plateau/{id}/supprimer/{idParticipation}")
 	@JsonView(Views.Participation.class)
